642994d5030675daff3303e4914a65d9fb1b98bf
Carlvin M <cmwange@innova.co.ke>
2021-12-30 20:07:59 +0300

    Switch statements

--- switchStatements/Program.cs
+++ switchStatements/Program.cs
@@ -5,8 +5,46 @@ namespace switchStatements // these are a special kind of if statemments that ca
     class Program
     {
         static void Main(string[] args)
+        {   
+
+            Console.WriteLine(GetDay(49));
+        }
+
+        //Method to convert numbers to days of the week.
+        static string GetDay(int dayNum)
         {
-            Console.WriteLine("Hello World!");
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid day number";
+                    break;
+            }
+
+
+            return dayName;
         }
     }
 }
