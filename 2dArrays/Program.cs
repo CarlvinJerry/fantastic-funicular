3af5f4e3f628e15bb2bdc2f13da5c7029127c4fb
Carlvin M <cmwange@innova.co.ke>
2022-01-09 12:58:41 +0300

    2d arrays

---
+++ 2dArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2dArrays //two dimesional arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid =
+            {
+                {1,2 },
+                {3,4 },
+                {5,6 }
+            };
+
+            //Accessing elelements in array...row i, col j
+            Console.WriteLine(numberGrid[1, 1]);
+        }
+    }
+}
