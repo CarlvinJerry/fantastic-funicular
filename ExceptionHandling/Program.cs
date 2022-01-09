ac19c4041d25faf933d47b8487a3d03558253061
Carlvin M <cmwange@innova.co.ke>
2022-01-09 13:19:16 +0300

    Exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter a number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch(Exception e) { 
+                Console.WriteLine(e.Message); //Avoid exception e to print out custome error messages
+
+            }
+            //catch (FormatException e)// Use specific exception types to only catch specific error types ie, FormatExceptio, DivideByZero Execption
+            //{
+            //    Console.WriteLine(e.Message); //Avoid exception e to print out custome error messages
+
+            //}
+            finally //this code gets executed no matter what error happens
+            {
+
+            }
+
+
+
+        }
+    }
+}
