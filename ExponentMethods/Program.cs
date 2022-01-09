2e42be571679e7cee7bab6dff478f52e9f8c7c8b
Carlvin M <cmwange@innova.co.ke>
2022-01-09 12:44:42 +0300

    For loops

---
+++ ExponentMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExponentMethods //Exponent functions that can take a base no and power number and power it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- ForLoops/Program.cs
+++ ForLoops/Program.cs
@@ -6,7 +6,26 @@ namespace ForLoops //These loops allow us to keep track of iterating variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int i = 1;
+            //while(i <= 5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            int[] luckuNumbers = { 4, 6, 7, 8, 15, 23, 42 }; //An array of ints
+
+            for (int i = 1; i <=5; i++)//requires three things unlike while loop: variable intitalization;loop condition;the exec action in the for loop
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //Lucku numbers
+            for (int i = 0; i < luckuNumbers.Length; i++)
+            {
+                Console.WriteLine(luckuNumbers[i]);
+                
+            }
         }
     }
 }
